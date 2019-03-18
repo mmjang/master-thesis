98239b388c5c14199d6adfaebf2ae694592425ca
mmjang <752515918@qq.com>
2019-03-18 21:49:46 +0800

    soil_surface

--- programs/plot/plotting.py
+++ programs/plot/plotting.py
@@ -1,7 +1,15 @@
 from matplotlib.font_manager import fontManager
+from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import rcParams
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy import fftpack
+
+from gprMax.constants import floattype
+from gprMax.constants import complextype
+from gprMax.utilities import round_value
+
+np.seterr(divide='raise')
 
 rcParams['font.family'] = 'serif'
 rcParams['font.serif'] = ['STSong']
@@ -57,3 +65,78 @@ plt.ylabel('ReLU(x)')
 #plt.title('阶跃函数', fontsize=10.5)
 plt.tight_layout()
 plt.savefig('pic/relu.pdf')
+
+########################################################################################
+#fractal soil
+surfacedims = (400,400)
+fractalsurface = np.zeros(surfacedims, dtype=complextype)
+weighting = (1, 1)
+v1 = np.array([weighting[0] * (surfacedims[0]) / 2, weighting[1] * (surfacedims[1]) / 2])
+R = np.random.RandomState()
+A = R.randn(surfacedims[0], surfacedims[1])
+
+sub_figure_width = (max_width * 0.95) / 2
+plt.figure(figsize=(sub_figure_width, sub_figure_width))
+plt.imshow(A, aspect='auto')
+plt.xlabel('x')
+plt.ylabel('y')
+#plt.imshow(fractalsurface, aspect='auto', cmap='Gray')
+plt.tight_layout()
+plt.savefig('pic/rough_surface_random.pdf')
+
+A = fftpack.fftn(A)
+A = fftpack.fftshift(A)
+
+plt.figure(figsize=(sub_figure_width, sub_figure_width))
+plt.imshow(np.real(A), aspect='auto')
+plt.xlabel('x')
+plt.ylabel('y')
+#plt.imshow(fractalsurface, aspect='auto', cmap='Gray')
+plt.tight_layout()
+plt.savefig('pic/rough_surface_fft.pdf')
+
+dimension = 1.5
+b = -(2 * dimension - 7) / 2
+##generate fractal 
+for i in range(surfacedims[0]):
+    for j in range(surfacedims[1]):
+        v2x = weighting[0] * i
+        v2y = weighting[1] * j
+        rr = ((v2x - v1[0])**2 + (v2y - v1[1])**2)**(1/2)
+        if rr == 0:
+            rr = 0.9
+        fractalsurface[i, j] = A[i, j] * 1 / (rr**b)
+
+plt.figure(figsize=(sub_figure_width, sub_figure_width))
+plt.imshow(np.real(fractalsurface), aspect='auto')
+plt.xlabel('x')
+plt.ylabel('y')
+#plt.imshow(fractalsurface, aspect='auto', cmap='Gray')
+plt.tight_layout()
+plt.savefig('pic/rough_surface_fft_fractal.pdf')
+##restore
+fractalsurface = fftpack.ifftshift(fractalsurface)
+fractalsurface = np.real(fftpack.ifftn(fractalsurface))
+
+plt.figure(figsize=(sub_figure_width, sub_figure_width))
+plt.imshow(np.real(fractalsurface), aspect='auto')
+plt.xlabel('x')
+plt.ylabel('y')
+#plt.imshow(fractalsurface, aspect='auto', cmap='Gray')
+plt.tight_layout()
+plt.savefig('pic/rough_surface_fractal.pdf')
+
+hf = plt.figure(figsize=(normal_width, normal_width))
+ha = hf.add_subplot(111, projection='3d')
+ha.set_aspect('equal')
+x = np.linspace(0, 1, surfacedims[0])
+y = np.linspace(0, 1, surfacedims[1])
+X, Y = np.meshgrid(x, y)
+ha.plot_surface(X, Y, fractalsurface, cmap='coolwarm')
+ha.set_xlabel('x')
+ha.set_ylabel('y')
+ha.set_zlabel('z')
+ha.set_zlim(-0.1,0.1)
+#plt.imshow(fractalsurface, aspect='auto', cmap='Gray')
+plt.tight_layout()
+plt.savefig('pic/rough_surface_fractal_3d.pdf')
